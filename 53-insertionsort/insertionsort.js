e8e4d97fd93c76838bababd6782f2a1ebddfefc2
John Chen <john@johnchen.co>
2016-08-25 23:14:15 -0400

    insertionsort

---
+++ 53-insertionsort/insertionsort.js
@@ -0,0 +1,12 @@
+function insertionSort (arr) {
+	for (var i = 1; i < arr.length; i++) {
+		var j = i;
+		while (j > 0 && arr[j] < arr[j -1]) {
+			var temp = arr[j];
+			arr[j] = arr[j - 1];
+			arr[j - 1] = temp;
+			j -= 1;
+		}
+	}
+	return arr;
+}
