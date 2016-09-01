a2015282406682168a25a082fb914d4e866984e4
coldshower <john@johnchen.co>
2016-09-01 16:36:30 -0400

    stability

---
+++ 65-stability/stability.js
@@ -0,0 +1,8 @@
+// sorts are stable if a list sorted one way will retain 
+// those results even if they are sorted a different way
+
+// stable
+// insertion sort
+// merge sort
+
+// those sorts don't move equal items past each other.
