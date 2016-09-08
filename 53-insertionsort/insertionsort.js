4a990f540730b7ac65511f7e479129831d20ed74
coldshower <john@johnchen.co>
2016-09-08 19:18:39 -0400

    start binary heap

--- 53-insertionsort/insertionsort.js
+++ 53-insertionsort/insertionsort.js
@@ -1,7 +1,7 @@
 function insertionSort (arr) {
 	for (var i = 1; i < arr.length; i++) {
 		var j = i;
-		while (j > 0 && arr[j] < arr[j -1]) {
+		while (j > 0 && arr[j] < arr[j - 1]) {
 			var temp = arr[j];
 			arr[j] = arr[j - 1];
 			arr[j - 1] = temp;
---
+++ 82-binaryheaps/binaryheaps.js
@@ -0,0 +1,6 @@
+// binary heap
+
+// uses array and indexes to represent a binary tree
+// starts information at index 1 for correct arithmetic
+// parent of k = k / 2
+// chilren of k = 2k and 2k + 1
